d5ecc02b18b455000512475bb043b21f4ae0fa49
Herredro <12672611+herredro@users.noreply.github.com>
2023-08-01 15:54:30 +0200

    clean

--- app.py
+++ app.py
@@ -15,11 +15,7 @@ def upload_file():
         
         df = pd.read_csv(file, delimiter=';')
         df = pandas_transform(df)
-        # Save the dataframe to a CSV string
         csv_string = df.to_csv(sep=';', index=False, quoting=0)
-        # data.to_csv('/mnt/data/powercloud-bc-2023-07-31_Gas_corrected.csv', sep=';', index=False)
-
-        # Create a generator for the CSV string
         def generate():
             yield csv_string
         
@@ -28,8 +24,6 @@ def upload_file():
     return render_template('upload.html')
 
 def pandas_transform(df):
-    # df_new = pd.DataFrame(columns=df.columns, data=[np.nan])
-    # df = pd.concat([df_new, df]).reset_index(drop=True)
     df['PLZ'] = df['PLZ'].astype(str).str.zfill(5)
     return df
 
